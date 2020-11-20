966cc1a8b03d116771a0b49f5efe22a5c3ab68c0
Anastasiia Sedova <anastasiia.sedova@univie.ac.at>
2020-11-20 11:53:15 +0100

    Revert "Feature/17 change python to 3.7"

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,21 +1,12 @@
 # Sample Test passing with nose and pytest
 import pytest
-import numpy as np
-from numpy.testing import assert_array_equal
 
 
 @pytest.fixture
-def init_majority_vote_function():
-    from knodle.final_label_decider.FinalLabelDecider import (
-        get_majority_vote_probabilities,
-    )
-
-    return get_majority_vote_probabilities
+def init_sum_function():
+    from knodle.sum import test_sum_function
+    return test_sum_function
 
 
-def test_pass(init_majority_vote_function):
-    mock_data = np.array(((1, 1, 1), (1, 1, 0), (1, 0, 0)))
-    output_classes = 2
-    correct_result = np.array(((0, 1), (0, 1), (1, 0)))
-    result = init_majority_vote_function(mock_data, output_classes)
-    assert_array_equal(correct_result, result)
+def test_pass(init_sum_function):
+    assert init_sum_function(1,1) == 2
