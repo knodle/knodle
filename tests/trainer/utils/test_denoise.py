73cf3f4f8022756c335a4d44c8f1a7b3fbf184cb
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2021-02-02 11:10:26 +0100

    Removed comments

--- tests/trainer/utils/test_denoise.py
+++ tests/trainer/utils/test_denoise.py
@@ -94,34 +94,3 @@ def test_denoise_knn():
 
     denoised_z = activate_neighbors(test_array, indices)
     assert_array_equal(denoised_z, right_result)
-
-# def test_denoise_knn_2():
-#     test_array = np.diag(np.ones((3,)))
-#     indices = np.array([
-#         np.array([0, 1, 2]),
-#         np.array([0]),
-#         np.array([1])
-#     ])
-#
-#     right_result = np.zeros((3, 3))
-#     right_result[0, :] = 1
-#     right_result[1, 0] = 1
-#     right_result[2, 1] = 1
-#
-#     denoised_z = activate_all_neighbors_2(test_array, indices)
-#     assert_array_equal(denoised_z, right_result)
-#
-#     test_array = np.diag(np.ones((3,)))
-#     indices = np.array([
-#         np.array([0, 1]),
-#         np.array([1, 2]),
-#         np.array([0, 2])
-#     ])
-#
-#     right_result = np.ones((3, 3))
-#     right_result[0, 2] = 0
-#     right_result[1, 0] = 0
-#     right_result[2, 1] = 0
-#
-#     denoised_z = activate_all_neighbors_2(test_array, indices)
-#     assert_array_equal(denoised_z, right_result)
