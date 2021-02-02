b7da50bd91987cc7309de3172c4143b998b0eef0
stephana93 <andreas.stephan@univie.ac.at>
2021-02-02 15:49:16 +0100

    Move majority to transformations and evaluation

--- knodle/trainer/utils/denoise.py
+++ knodle/trainer/utils/denoise.py
@@ -10,7 +10,7 @@ def sample_majority(probs: np.array, random_label: bool = True, default_label: i
         return np.argmax(probs)
     else:
         if random_label:
-            return randint(0, probs.shape[0] - 1)
+            return randint(0, probs.shape[0] - 1)  # TODO only sample from top classes; should work on
         return default_label
 
 
@@ -33,11 +33,7 @@ def get_majority_vote_labels(
             return np.argmax(row)
         else:
             if no_rule_label is None:
-                a = randint(0, rule_counts_probs.shape[1] - 1)
-                if a >= 2:
-                    print(a)
-                    print(rule_counts_probs.shape)
-                return a
+                return randint(0, rule_counts_probs.shape[1] - 1)
             return no_rule_label
 
     majority_labels = np.apply_along_axis(row_majority, axis=1, arr=rule_counts_probs)
@@ -45,23 +41,26 @@ def get_majority_vote_labels(
 
 
 def get_majority_vote_probs(
-        rule_matches_z: np.ndarray, mapping_rules_labels_t: np.ndarray
+        rule_matches_z: np.ndarray, mapping_rules_labels_t: np.ndarray, other_class: int = None
 ) -> np.ndarray:
     """
-    This function calculates a majority vote probability for all rule_matches_z. First rule counts will be
-    calculated,
-    then a probability will be calculated by dividing the values row-wise with the sum. To counteract zero
-    division
-    all nan values are set to zero.
+    This function calculates a majority vote probability for all rule_matches_z. The difference from simple
+    get_majority_vote_probs function is the following: samples, where no rules matched (that is, all elements in
+    the corresponding raw in rule_matches_z matrix equal 0), are assigned to no_match_class (that is, a value in the
+    corresponding column in rule_counts_probs matrix is changed to 1).
+
     Args:
         rule_matches_z: Binary encoded array of which rules matched. Shape: instances x rules
         mapping_rules_labels_t: Mapping of rules to labels, binary encoded. Shape: rules x classes
-    Returns: Array with majority vote decision. Shape: instances x classes
+        other_class: Class which is chosen, if no function is hitting.
+    Returns: Array with majority vote probabilities. Shape: instances x classes
     """
     if rule_matches_z.shape[1] != mapping_rules_labels_t.shape[0]:
         raise ValueError("Dimensions mismatch!")
 
     rule_counts = np.matmul(rule_matches_z, mapping_rules_labels_t)
+    if other_class is not None:
+        rule_counts[~rule_counts.any(axis=1), other_class] = 1
     rule_counts_probs = rule_counts / rule_counts.sum(axis=1).reshape(-1, 1)
 
     rule_counts_probs[np.isnan(rule_counts_probs)] = 0
@@ -69,7 +68,7 @@ def get_majority_vote_probs(
 
 
 def get_majority_vote_probs_with_no_rel(
-        rule_matches_z: np.ndarray, mapping_rules_labels_t: np.ndarray, no_match_class: int
+        rule_matches_z: np.ndarray, mapping_rules_labels_t: np.ndarray, no_match_class: int = None
 ) -> np.ndarray:
     """
     This function calculates a majority vote probability for all rule_matches_z. The difference from simple
@@ -86,43 +85,12 @@ def get_majority_vote_probs_with_no_rel(
         raise ValueError("Dimensions mismatch!")
 
     rule_counts = np.matmul(rule_matches_z, mapping_rules_labels_t)
-    rule_counts[~rule_counts.any(axis=1), no_match_class] = 1
+    if no_match_class is not None:
+        rule_counts[~rule_counts.any(axis=1), no_match_class] = 1
     rule_counts_probs = rule_counts / rule_counts.sum(axis=1).reshape(-1, 1)
 
     rule_counts_probs[np.isnan(rule_counts_probs)] = 0
     return rule_counts_probs
-
-
-# def activate_all_neighbors(
-#         rule_matches_z: np.ndarray, indices: np.ndarray
-# ) -> np.ndarray:
-#     """
-#     Find all closest neighbors and take the same label ids
-#     Args:
-#         rule_matches_z: All rule matches. Shape: instances x rules
-#         indices: Neighbor indices from knn
-#     Returns:
-#     """
-#     new_lfs_array = np.full(rule_matches_z.shape, fill_value=0)
-#
-#     for index, lf in enumerate(rule_matches_z):
-#
-#         try:
-#             matched_lfs = np.where(lf != 0)[0]
-#             if len(matched_lfs) == 0:
-#                 continue
-#             matched_lfs = matched_lfs[:, np.newaxis]
-#             neighbors = indices[index]
-#             to_replace = new_lfs_array[neighbors, matched_lfs]
-#             label_matched_lfs = lf[matched_lfs][:, 0]
-#             tiled_labels = np.tile(
-#                 np.array(label_matched_lfs), (to_replace.shape[1], 1)
-#             ).transpose()
-#             new_lfs_array[neighbors, matched_lfs] = tiled_labels
-#         except IndexError:
-#             pass
-#
-#     return new_lfs_array
 
 
 def activate_neighbors(
