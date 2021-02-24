030dd6cc9c7336616b4a24ba0581f71d97d7f97c
marina-sp <speranskaya@cis.lmu.de>
2021-02-24 18:34:23 +0100

    fix tacred test; adjust script for testing only

--- tutorials/crossweigh_weighing_example/crossweigh_training_example_for_tacred_params_console.py
+++ tutorials/crossweigh_weighing_example/crossweigh_training_example_for_tacred_params_console.py
@@ -19,6 +19,7 @@ from knodle.trainer.crossweigh_weighing.crossweigh_bert import CrossWeigh
 from knodle.trainer.crossweigh_weighing.crossweigh_denoising_config import CrossWeighDenoisingConfig
 from knodle.trainer.crossweigh_weighing.crossweigh_trainer_config import CrossWeighTrainerConfig
 from tutorials.crossweigh_weighing_example.utils import vocab_and_vectors
+from knodle.trainer.utils.utils import check_and_return_device
 
 
 NUM_CLASSES = 42
@@ -38,8 +39,8 @@ def train_crossweigh(
         cw_epochs: int = None,
         weight_reducing_rate: int = None,
         samples_start_weights: int = None,
-        epochs: int = None
-
+        epochs: int = None,
+        load_epoch: int = 0
 ) -> None:
     """
     Training the model with CrossWeigh model denoising
@@ -61,13 +62,14 @@ def train_crossweigh(
     word2id, word_embedding_matrix = vocab_and_vectors(path_to_emb)
     tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 
-    train_df, dev_df, test_df, rule_matches_z, mapping_rules_labels_t = read_train_dev_test(path_to_data)
+    #train_df, dev_df, test_df, rule_matches_z, mapping_rules_labels_t = read_train_dev_test(path_to_data)
+    test_df = read_test(path_to_data)
 
-    train_input_x_glove = get_glove_encoded_features(train_df, word2id, 0)
-    train_input_x_bert = get_bert_encoded_features(train_df, tokenizer, 0)
+    #train_input_x_glove = get_glove_encoded_features(train_df, word2id, 0)
+    #train_input_x_bert = get_bert_encoded_features(train_df, tokenizer, 0)
 
-    dev_dataset_bert = get_bert_encoded_features(dev_df, tokenizer, 0)
-    dev_labels = torch.LongTensor(list(dev_df.iloc[:, 3]))
+    #dev_dataset_bert = get_bert_encoded_features(dev_df, tokenizer, 0)
+    #dev_labels = torch.LongTensor(list(dev_df.iloc[:, 3]))
 
     test_dataset_bert = get_bert_encoded_features(test_df, tokenizer, 0)
     test_labels = torch.LongTensor(list(test_df.iloc[:, 3]))
@@ -91,7 +93,17 @@ def train_crossweigh(
     )
 
     model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=NUM_CLASSES)
-
+    model_path = os.path.join(path_to_weights, "trained_models", f"model_epoch_{load_epoch}.pth")
+    model.load_state_dict(torch.load(model_path))     
+    
+    print()
+    print("#######################")
+    print("Model state has been loaded from {}".format(model_path))
+    print("#######################")
+    print()
+    
+    torch.cuda.empty_cache()
+    
     custom_crossweigh_denoising_config = CrossWeighDenoisingConfig(
         model=model_cw,
         crossweigh_partitions=cw_partitions,
@@ -102,6 +114,7 @@ def train_crossweigh(
         samples_start_weights=samples_start_weights,
         optimizer_=torch.optim.Adam(model.parameters(), lr=cw_lr),
         output_classes=NUM_CLASSES,
+        batch_size=512,
         filter_empty_probs=False,
         no_match_class_label=NO_MATCH_CLASS_LABEL
     )
@@ -111,29 +124,30 @@ def train_crossweigh(
         output_classes=NUM_CLASSES,
         optimizer_=AdamW(model.parameters(), lr=1e-4),
         epochs=epochs,
+        batch_size=32,
         filter_empty_probs=False,
         no_match_class_label=NO_MATCH_CLASS_LABEL
     )
 
-    trainer = CrossWeigh(
-        model=model,
-        rule_assignments_t=mapping_rules_labels_t,
-        inputs_x=train_input_x_bert,
-        rule_matches_z=rule_matches_z,
-        # dev_features=dev_dataset_bert,
-        # dev_labels=dev_labels,
-        cw_model=model_cw,
-        cw_inputs_x=train_input_x_glove,
-        evaluation_method="tacred",
-        dev_labels_ids=labels2ids,
-        path_to_weights=path_to_weights,
-        denoising_config=custom_crossweigh_denoising_config,
-        trainer_config=custom_crossweigh_trainer_config
-    )
-    trainer.train()
-    # print("Testing on the test dataset....")
-    # metrics = test_tacred_dataset(model, trainer, test_dataset_bert, test_labels, labels2ids)
-    # metrics = test(model, optimizer, features_dataset: TensorDataset, labels: TensorDataset, device)
+#     trainer = CrossWeigh(
+#         model=model,
+#         rule_assignments_t=mapping_rules_labels_t,
+#         inputs_x=train_input_x_bert,
+#         rule_matches_z=rule_matches_z,
+#         # dev_features=dev_dataset_bert,
+#         # dev_labels=dev_labels,
+#         cw_model=model_cw,
+#         cw_inputs_x=train_input_x_glove,
+#         evaluation_method="tacred",
+#         dev_labels_ids=labels2ids,
+#         path_to_weights=path_to_weights,
+#         denoising_config=custom_crossweigh_denoising_config,
+#         trainer_config=custom_crossweigh_trainer_config
+#     )
+    #trainer.train()
+    print("Testing on the test dataset....")
+    metrics = test_tacred_dataset(model, test_dataset_bert, test_labels, labels2ids, device=check_and_return_device())
+    #metrics = test(model, optimizer, features_dataset: TensorDataset, labels: TensorDataset, device)
 
     # tb.add_hparams(
     #     {"cw_lr": cw_lr,
@@ -151,13 +165,18 @@ def train_crossweigh(
 
 
 def read_train_dev_test(target_path: str):
-    train_df = load(os.path.join(target_path, 'df_train.lib'))
-    dev_df = load(os.path.join(target_path, 'df_dev.lib'))
+    #train_df = load(os.path.join(target_path, 'df_train.lib'))
+    #dev_df = load(os.path.join(target_path, 'df_dev.lib'))
     test_df = load(os.path.join(target_path, 'df_test.lib'))
-    train_rule_matches_z = load(os.path.join(target_path, 'train_rule_matches_z.lib'))
-    mapping_rules_labels_t = load(os.path.join(target_path, 'mapping_rules_labels.lib'))
+    #train_rule_matches_z = load(os.path.join(target_path, 'train_rule_matches_z.lib'))
+    #mapping_rules_labels_t = load(os.path.join(target_path, 'mapping_rules_labels.lib'))
 
-    return train_df, dev_df, test_df, train_rule_matches_z, mapping_rules_labels_t
+    #return train_df, dev_df, test_df, train_rule_matches_z, mapping_rules_labels_t
+    return test_df
+
+def read_test(target_path: str):
+    test_df = load(os.path.join(target_path, 'df_test.lib'))
+    return test_df
 
 
 def get_glove_encoded_features(input_data: pd.DataFrame, word2id: dict, column_num: int) -> TensorDataset:
@@ -206,17 +225,39 @@ def add_padding(tokens: list, maxlen: int) -> list:
     return padded_tokens
 
 
-def test_tacred_dataset(model, trainer, test_features: TensorDataset, test_labels: Tensor, labels2ids: Dict) -> Dict:
-    feature_labels_dataset = TensorDataset(test_features.tensors[0], test_labels)
-    feature_labels_dataloader = trainer._make_dataloader(feature_labels_dataset)
+from torch.utils.data import TensorDataset, DataLoader
 
+def test_tacred_dataset(model, test_features: TensorDataset, test_labels: Tensor, labels2ids: Dict, device) -> Dict:
+    feature_labels_dataset = TensorDataset(*test_features.tensors[:2], test_labels)
+    
+    feature_labels_dataloader = DataLoader(
+            feature_labels_dataset,
+            batch_size=32,
+            drop_last=False,
+            shuffle=False,
+    )
+    
+    model.to(device)
     model.eval()
-    all_predictions, all_labels = torch.Tensor(), torch.Tensor()
-    for features, labels in feature_labels_dataloader:
-        outputs = model(features)
-        _, predicted = torch.max(outputs, 1)
-        all_predictions = torch.cat([all_predictions, predicted])
-        all_labels = torch.cat([all_labels, labels])
+    all_predictions, all_labels = torch.LongTensor().to(device), torch.LongTensor().to(device)
+    for input_ids_batch, attention_mask_batch, labels in feature_labels_dataloader:
+        features = {
+            "input_ids": input_ids_batch.to(device),
+            "attention_mask": attention_mask_batch.to(device)
+        }
+        labels = labels.to(device)
+
+        predictions = model(**features)[0]
+
+        _ , predicted = torch.max(predictions, 1)
+        all_predictions = torch.cat([all_predictions, predicted.long()])
+        all_labels = torch.cat([all_labels, labels.long()])
+
+#     for features, labels in feature_labels_dataloader:
+#         outputs = model(features)
+#         _, predicted = torch.max(outputs, 1)
+#         all_predictions = torch.cat([all_predictions, predicted])
+#         all_labels = torch.cat([all_labels, labels])
 
     predictions_idx, test_labels_idx = (all_predictions.detach().type(torch.IntTensor).tolist(),
                                         all_labels.detach().type(torch.IntTensor).tolist())
@@ -244,7 +285,9 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", help="")
 
     args = parser.parse_args()
-
+        
+    print(args)
+    
     train_crossweigh(
         args.path_to_data,
         args.sample_weights,
@@ -256,5 +299,23 @@ if __name__ == "__main__":
         args.cw_epochs,
         args.weight_reducing_rate,
         args.samples_start_weights,
-        args.epochs
+        args.epochs,
+        load_epoch=0
+    )
+    
+    torch.cuda.empty_cache()
+    
+    train_crossweigh(
+        args.path_to_data,
+        args.sample_weights,
+        args.path_to_emb,
+        args.path_to_labels,
+        args.cw_lr,
+        args.cw_partitions,
+        args.cw_folds,
+        args.cw_epochs,
+        args.weight_reducing_rate,
+        args.samples_start_weights,
+        args.epochs,
+        load_epoch=1
     )
