a39f4452800d6cca8a1ef59e341dd143f7b64594
marina-sp <19635192+marina-sp@users.noreply.github.com>
2021-03-18 15:15:48 +0300

    one version file

--- knodle/__init__.py
+++ knodle/__init__.py
@@ -1,7 +1,5 @@
 """knodle - Knowledge infused deep learning framework"""
 
-
-__version__ = "0.1.0"
 __author__ = "knodle <knodle@cs.univie.ac.at>"
 __all__ = []
 
@@ -12,6 +10,6 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 logger.info("Initalized logger")
