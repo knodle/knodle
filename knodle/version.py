cd3009551b78ec28996745dcfcd66461b849ca57
marina-sp <19635192+marina-sp@users.noreply.github.com>
2021-03-19 19:06:21 +0300

    upload to pypi

--- knodle/version.py
+++ knodle/version.py
@@ -1,3 +1,3 @@
-# This is a placeholder file.
-# The updated version info is generated during package build via github action.
+# This is a placeholder file and is not supposed to be maintained.
+# This file will be replaced with an up-to-date value during package build via github action.
 __version__ = '0.0.0'
