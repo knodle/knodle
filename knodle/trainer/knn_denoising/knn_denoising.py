bbb4ab9d07acd4273063d0e1f3f64db645a4d741
marina-sp <19635192+marina-sp@users.noreply.github.com>
2021-03-19 20:21:04 +0300

    remove gc

--- knodle/trainer/knn_denoising/knn_denoising.py
+++ knodle/trainer/knn_denoising/knn_denoising.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import gc
 
 import joblib
 import numpy as np
@@ -13,6 +12,8 @@ from annoy import AnnoyIndex
 from knodle.transformation.majority import input_to_majority_vote_input
 from knodle.transformation.torch_input import input_labels_to_tensordataset
 
+from knodle.trainer.trainer import Trainer
+from knodle.trainer.auto_trainer import AutoTrainer
 from knodle.trainer.baseline.no_denoising import NoDenoisingTrainer
 from knodle.trainer.knn_denoising.config import KNNConfig
 from knodle.trainer.utils.denoise import activate_neighbors
@@ -20,6 +21,7 @@ from knodle.trainer.utils.denoise import activate_neighbors
 logger = logging.getLogger(__name__)
 
 
+@AutoTrainer.register('knn_trainer')
 class KnnDenoisingTrainer(NoDenoisingTrainer):
     def __init__(
             self,
@@ -35,27 +37,22 @@ class KnnDenoisingTrainer(NoDenoisingTrainer):
         else:
             self.knn_feature_matrix = knn_feature_matrix
 
+        self.rule_matches_z = self.rule_matches_z.astype(np.int8)
+        self.mapping_rules_labels_t = self.mapping_rules_labels_t.astype(np.int8)
+
     def train(self):
         """
         This function gets final labels with a majority vote approach and trains the provided model.
         """
 
-        denoised_rule_matches_z = self._knn_denoise_rule_matches()
-        print(f"denoised_rule: {denoised_rule_matches_z.shape}")
-        
-        self.rule_matches_z = None
-        gc.collect()
-        
-        print(f"denoised_rule: {denoised_rule_matches_z.shape}")
-        print(f"non-zero rows: {(denoised_rule_matches_z.sum(-1) != 0).sum()}")
-        
+        self._knn_denoise_rule_matches()
+
         model_input_x, label_probs = input_to_majority_vote_input(
-            self.model_input_x, denoised_rule_matches_z, self.mapping_rules_labels_t.astype(np.int64),
+            self.model_input_x, self.rule_matches_z, self.mapping_rules_labels_t.astype(np.int64),
             filter_non_labelled=self.trainer_config.filter_non_labelled,
             other_class_id=self.trainer_config.other_class_id
         )
 
-        print(f"model input: {model_input_x.tensors[0].shape} label_probs: {label_probs.shape}")
         feature_label_dataset = input_labels_to_tensordataset(model_input_x, label_probs)
         feature_label_dataloader = self._make_dataloader(feature_label_dataset)
 
@@ -93,7 +90,6 @@ class KnnDenoisingTrainer(NoDenoisingTrainer):
         if self.trainer_config.use_approximation:
             # use annoy fast ANN
             if k is not None:
-                indices = []
                 knn_matrix_shape = self.knn_feature_matrix.shape
                 
                 logger.info("Creating annoy index...")
@@ -102,12 +98,10 @@ class KnnDenoisingTrainer(NoDenoisingTrainer):
                     if not ignore[i]:
                         t.add_item(i, v)
 
-                t.build(10, n_jobs=10)
-                
-                # free RAM
+                t.build(10, n_jobs=self.trainer_config.n_jobs)
+
                 self.knn_feature_matrix = None
-                gc.collect()
-                
+
                 logger.info("Retrieving neighbor indices...")
                 indices = ( # make a generator: no memory is allocated at this moment
                     np.array(t.get_nns_by_item(i, k, search_k=-1, include_distances=False)) 
@@ -115,30 +109,33 @@ class KnnDenoisingTrainer(NoDenoisingTrainer):
                     for i in range(knn_matrix_shape[0])
                 )
             else:
+                # possible radius implementation; delete error in config then
                 pass
         else:
             # use standard precise kNN
             if k is not None:
                 logger.info("Creating NN index...")
-                neighbors = NearestNeighbors(n_neighbors=k, n_jobs=10).fit(self.knn_feature_matrix)
+                neighbors = NearestNeighbors(n_neighbors=k, n_jobs=self.trainer_config.n_jobs)\
+                    .fit(self.knn_feature_matrix)
                 logger.info("Retrieving neighbor indices...")
                 indices = neighbors.kneighbors(self.knn_feature_matrix, n_neighbors=k, return_distance=False)
             else:
                 logger.info("Creating NN index...")
-                neighbors = NearestNeighbors(radius=self.trainer_config.radius, n_jobs=10).fit(self.knn_feature_matrix)
+                neighbors = NearestNeighbors(radius=self.trainer_config.radius, n_jobs=self.trainer_config.n_jobs)\
+                    .fit(self.knn_feature_matrix)
                 logger.info("Retrieving neighbor indices...")
                 indices = neighbors.radius_neighbors(self.knn_feature_matrix, return_distance=False)
                 
         # activate matches.
         logger.info("Activating neighbors...")
-        denoised_rule_matches_z = activate_neighbors(self.rule_matches_z, indices)
+        self.rule_matches_z = activate_neighbors(self.rule_matches_z, indices)
 
         # save data for caching
         if cache_dir is not None:
             os.makedirs(cache_dir, exist_ok=True)
-            joblib.dump(denoised_rule_matches_z, cache_file)
+            joblib.dump(self.rule_matches_z, cache_file)
 
-        return denoised_rule_matches_z
+        return self.rule_matches_z
 
     def print_step_update(self, step: int, max_steps: int):
         if step % 40 == 0 and not step == 0:
