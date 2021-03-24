e07a00ac6612c2760477a7079ab2beb6848a1d8e
marina-sp <19635192+marina-sp@users.noreply.github.com>
2021-03-24 20:11:16 +0300

    revert logging level

--- knodle/__init__.py
+++ knodle/__init__.py
@@ -12,6 +12,6 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 logger.info("Initalized logger")
