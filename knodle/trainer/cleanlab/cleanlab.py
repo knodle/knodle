83c7907598328da9c4b51e9bde52b2a4ae6d18a6
agsedova <anastasiia.sedova@gmail.com>
2021-04-14 20:10:24 +0300

    entities used for testing the algorithm are deleted CL tutorial is updated

--- knodle/trainer/cleanlab/cleanlab.py
+++ knodle/trainer/cleanlab/cleanlab.py
@@ -18,8 +18,6 @@ class CleanLabTrainer(MajorityVoteTrainer):
         if kwargs.get("trainer_config", None) is None:
             kwargs["trainer_config"] = CleanLabConfig(optimizer=SGD, lr=0.001)
         super().__init__(**kwargs)
-        self.test_x = test_x
-        self.test_y = test_y
 
     def train(
             self,
@@ -27,17 +25,6 @@ class CleanLabTrainer(MajorityVoteTrainer):
             dev_model_input_x: TensorDataset = None, dev_gold_labels_y: TensorDataset = None
     ) -> None:
         self._load_train_params(model_input_x, rule_matches_z, dev_model_input_x, dev_gold_labels_y)
-
-        # turn input to the CL-compatible format
-        model_input_x_numpy = input_to_2_dim_numpy(self.model_input_x)
-
-        # calculate labels based on t and z
-        noisy_y_train = np.argmax(
-            z_t_matrices_to_majority_vote_probs(self.rule_matches_z, self.mapping_rules_labels_t), axis=1
-        )
-
-        # uncomment the following line if you want to calculate the baseline
-        # self.calculate_baseline(model_input_x_numpy, noisy_y_train)
 
         # since CL accepts only sklearn.classifier compliant class, we wraps the PyTorch model
         self.model = NeuralNetClassifier(
@@ -48,8 +35,15 @@ class CleanLabTrainer(MajorityVoteTrainer):
             max_epochs=self.trainer_config.epochs,
             batch_size=self.trainer_config.batch_size,
             train_split=None,
-            # callbacks="disable",
+            callbacks="disable",
             device=self.trainer_config.device
+        )
+        # turn input to the CL-compatible format
+        model_input_x_numpy = input_to_2_dim_numpy(self.model_input_x)
+
+        # calculate labels based on t and z
+        noisy_y_train = np.argmax(
+            z_t_matrices_to_majority_vote_probs(self.rule_matches_z, self.mapping_rules_labels_t), axis=1
         )
 
         # CL denoising and training
@@ -62,23 +56,3 @@ class CleanLabTrainer(MajorityVoteTrainer):
             n_jobs=self.trainer_config.n_jobs
         )
         _ = rp.fit(model_input_x_numpy, noisy_y_train)
-        pred = rp.predict(input_to_2_dim_numpy(self.test_x))
-        print("Test accuracy:", round(accuracy_score(pred, self.test_y.tensors[0].numpy()), 2))
-
-    def calculate_baseline(self, model_input_x: np.ndarray, noisy_y_train: np.ndarray) -> None:
-        baseline_model = NeuralNetClassifier(
-            self.model,
-            criterion=self.trainer_config.criterion,
-            optimizer=self.trainer_config.optimizer,
-            lr=self.trainer_config.lr,
-            max_epochs=self.trainer_config.epochs,
-            batch_size=self.trainer_config.batch_size,
-            train_split=None,
-            # callbacks="disable",
-            device=self.trainer_config.device
-        )
-
-        baseline_model.fit(model_input_x, noisy_y_train)
-        baseline_pred_baseline = baseline_model.predict(input_to_2_dim_numpy(self.test_x))
-        print("Baseline test accuracy:", round(accuracy_score(baseline_pred_baseline, self.test_y.tensors[0].numpy()), 2))
-
