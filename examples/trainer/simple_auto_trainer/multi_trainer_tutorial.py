b3b855befe9d09c371408bd4c3202da040f87c99
Elisabeth <elisabeth.katharina.wimmer@univie.ac.at>
2021-10-14 10:26:48 +0200

    adjusted knodle and minio download link in several files

--- examples/trainer/simple_auto_trainer/multi_trainer_tutorial.py
+++ examples/trainer/simple_auto_trainer/multi_trainer_tutorial.py
@@ -1,3 +1,26 @@
+
+# Define constants
+imdb_data_dir = os.path.join(os.getcwd(), "datasets", "spam")
+processed_data_dir = os.path.join(imdb_data_dir, "processed")
+os.makedirs(processed_data_dir, exist_ok=True)
+
+# Download data
+client = Minio("knodle.cc", secure=False)
+files = [
+    "df_train.csv", "df_test.csv",
+    "train_rule_matches_z.lib", "test_rule_matches_z.lib",
+    "mapping_rules_labels_t.lib"
+]
+for file in tqdm(files):
+    client.fget_object(
+        bucket_name="knodle",
+        object_name=os.path.join("datasets/spam/processed/", file),
+        file_path=os.path.join(processed_data_dir, file),
+    )
+
+# Load data into memory
+df_train = pd.read_csv(os.path.join(processed_data_dir, "df_train.csv"))
+df_test = pd.read_csv(os.path.join(processed_data_dir, "df_test.csv"))
 import os
 from torch import Tensor
 from tqdm.auto import tqdm
@@ -33,29 +56,6 @@ def np_array_to_tensor_dataset(x: np.ndarray) -> TensorDataset:
     x = TensorDataset(x)
     return x
 
-
-# Define constants
-imdb_data_dir = os.path.join(os.getcwd(), "datasets", "spam")
-processed_data_dir = os.path.join(imdb_data_dir, "processed")
-os.makedirs(processed_data_dir, exist_ok=True)
-
-# Download data
-client = Minio("knodle.dm.univie.ac.at", secure=False)
-files = [
-    "df_train.csv", "df_test.csv",
-    "train_rule_matches_z.lib", "test_rule_matches_z.lib",
-    "mapping_rules_labels_t.lib"
-]
-for file in tqdm(files):
-    client.fget_object(
-        bucket_name="knodle",
-        object_name=os.path.join("datasets/spam/processed", file),
-        file_path=os.path.join(processed_data_dir, file),
-    )
-
-# Load data into memory
-df_train = pd.read_csv(os.path.join(processed_data_dir, "df_train.csv"))
-df_test = pd.read_csv(os.path.join(processed_data_dir, "df_test.csv"))
 
 mapping_rules_labels_t = joblib.load(os.path.join(processed_data_dir, "mapping_rules_labels_t.lib"))
 
