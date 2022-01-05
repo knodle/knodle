eb56895a8c2a567c4c6c64d3a2003d82b4a65625
elisabear <elisabear@users.noreply.github.com>
2022-01-05 21:24:14 +0100

    deleted comments in arg_parser

--- knodle/labeler/CheXpert/args/arg_parser.py
+++ knodle/labeler/CheXpert/args/arg_parser.py
@@ -9,16 +9,6 @@ class ArgParser(object):
         """Initialize argument parser."""
         parser = argparse.ArgumentParser()
 
-        # # Input report parameters.
-        # parser.add_argument('--reports_path',
-        #                     required=True,
-        #                     help='Path to file with radiology reports.')
-
-        # Output parameters.
-        # parser.add_argument('--output_path',
-        #                     default='labeled_reports.csv',
-        #                     help='Output path to write labels to.')
-
         # Misc.
         parser.add_argument('-v', '--verbose',
                             action='store_true',
@@ -29,8 +19,5 @@ class ArgParser(object):
     def parse_args(self):
         """Parse and validate the supplied arguments."""
         args = self.parser.parse_args()
-
-        #args.reports_path = Path(args.reports_path)
-        #args.output_path = Path(args.output_path)
 
         return args
